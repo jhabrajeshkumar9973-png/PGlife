c87b417c5e1086ccd5353928139b86f10ad375a2
Brajesh kumar jha <jhabrajeshkumar9973@gmail.com>
2026-07-03 23:47:36 +0530

    fixed database exact  connection

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect(
     "bn9qtfihhqmjmmsizz7v-mysql.services.clever-cloud.com",
     "usy0m5lemjkk30lh",
-    "_yeYn6lShcCi9AjD10d4S",
+    "yeYn6lShcCi9AjD10d4S",
     "bn9qtfihhqmjmmsizz7v"
 );
 
